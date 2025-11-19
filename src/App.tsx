efa6bc201d6baa1554a83bfc8a8fc46cf78411f9
Mike Casey <hi@mcasey.xyz>
2025-11-18 22:05:19 -0500

    Refine Mini-App to official SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react'
 import HerodotusExplorer from './components/HerodotusExplorer.jsx'
+import Footer from './components/Footer.jsx'
 import './App.css'
+// 1. Import the SDK
+import { sdk } from '@farcaster/miniapp-sdk'
 
 function App() {
+  useEffect(() => {
+    const init = async () => {
+      // 2. Tell Farcaster we are ready (Removes the splash screen)
+      await sdk.actions.ready();
+    };
+    init();
+  }, []);
+
   return (
-    <div className="min-h-screen bg-[#F5F3EF]">
+    <>
       <HerodotusExplorer />
-    </div>
+      <Footer />
+    </>
   )
 }
 
